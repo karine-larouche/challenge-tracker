a4534bc19ca1bc18746f351f0ac22d2adc829f43
Unknown <karine.larouche14@gmail.com>
2018-07-11 14:11:09 -0400

    Setup rematch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { init } from '@rematch/core';
+import models from './models';
 import logo from './logo.svg';
 import './App.css';
 
+const store = init({
+  models,
+});
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+  <Provider store={store}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  </Provider>
 );
 
 export default App;
