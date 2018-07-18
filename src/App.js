32efc340a1a6bcfd01fc3237901e0021b7be6837
Unknown <karine.larouche14@gmail.com>
2018-07-18 14:55:00 -0400

    Setup react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { init } from '@rematch/core';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 import models from './models';
-import logo from './logo.svg';
 import './App.css';
 
 const store = init({
@@ -11,15 +12,12 @@ const store = init({
 
 const App = () => (
   <Provider store={store}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <div>Match</div>} />
+        <Route render={() => <div>Miss</div>} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
 );
 
