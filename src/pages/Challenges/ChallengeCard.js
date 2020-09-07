68727fc2304c938bac7188769b8d108c5301c542
Karine Larouche <karine.larouche14@gmail.com>
2020-09-07 11:31:19 -0400

    Use colour-utils library

--- src/pages/Challenges/ChallengeCard.js
+++ src/pages/Challenges/ChallengeCard.js
@@ -4,15 +4,15 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Typography } from '@material-ui/core';
+import { mix } from 'colour-utils';
 import { propTypesChallenge } from '../../utils/propTypes';
-import { mix } from '../../utils/colorUtils';
 
 const styles = theme => ({
   card: {
     height: 128,
     cursor: 'pointer',
     '&:hover': {
-      backgroundColor: mix('#000000', theme.palette.background.paper, 2),
+      backgroundColor: mix('#000000', theme.palette.background.paper, 0.98),
     },
   },
 });
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import * as colors from '@material-ui/core/colors';
-import { mix } from './utils/colorUtils';
+import { mix } from 'colour-utils';
 
 const white = '#ffffff';
 const black = '#000000';
@@ -10,17 +10,17 @@ const primary = colors.lightBlue[900];
 const theme = createMuiTheme({
   palette: {
     primary: {
-      veryLight: mix(white, primary, 70),
-      light: mix(white, primary, 35),
+      veryLight: mix(white, primary, 0.3),
+      light: mix(white, primary, 0.65),
       main: primary,
-      dark: mix(black, primary, 30),
+      dark: mix(black, primary, 0.7),
     },
     secondary: {
       main: colors.yellow[700],
     },
     background: {
       paper: white,
-      default: mix(primary, colors.grey[100], 3),
+      default: mix(primary, colors.grey[100], 0.97),
     },
   },
   typography: {
